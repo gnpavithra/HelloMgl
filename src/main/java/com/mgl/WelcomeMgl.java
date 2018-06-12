b57bd3c71e94d385cec4ae0fce70e33326992a06
pavithra <gnpavithra2017@gmail.com>
2018-06-12 14:34:16 +0530

    commit new mgl

--- src/main/java/com/mgl/WelcomeMgl.java
+++ src/main/java/com/mgl/WelcomeMgl.java
@@ -1,7 +1,8 @@
 package com.mgl;
 
 public class WelcomeMgl {
-public static void main(String[] args) {
-	System.out.println("welcome to MGL!");
-}
+	public static void main(String[] args) {
+		System.out.println("welcome to MGL!");
+		System.out.println("byeeee");
+	}
 }
